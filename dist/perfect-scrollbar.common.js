39681df874e992aeeed67011eb3a1c6eee9a9056
Adam Kell <ackell@gmail.com>
2017-12-07 17:14:56 -0800

    c'mon

--- dist/perfect-scrollbar.common.js
+++ dist/perfect-scrollbar.common.js
@@ -500,15 +500,13 @@ var clickRail = function(i) {
 };
 
 var snapValue = function(value, snapValue) {
-
   // play w this?
   var boost = 0; //0.4 * (value > element.scrollTop ? 1 : -1)
-  snapped = Math.round((value / snapValue) + boost) * snapValue;
+  var snapped = Math.round((value / snapValue) + boost) * snapValue;
 
   console.log('snapValue', value, snapped);
 
   return snapped    
-
 };
 
 var dragThumb = function(i) {
--- src/snap-value.js
+++ src/snap-value.js
@@ -1,11 +1,9 @@
 export default function(value, snapValue) {
-
   // play w this?
-  var boost = 0; //0.4 * (value > element.scrollTop ? 1 : -1)
-  snapped = Math.round((value / snapValue) + boost) * snapValue;
+  let boost = 0; //0.4 * (value > element.scrollTop ? 1 : -1)
+  let snapped = Math.round((value / snapValue) + boost) * snapValue;
 
   console.log('snapValue', value, snapped);
 
   return snapped    
-
 }
